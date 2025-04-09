94e6ffade446dcf03e45844ab91a5ffaf1339473
dimao36 <09886543a@gmail.com>
2025-04-09 10:22:07 +0300

    js and css minify

--- gulpfile.js
+++ gulpfile.js
@@ -23,12 +23,32 @@
 
 const gulp = require('gulp');
 
-exports.cloneFiles = async () => {
+exports.cloneHTML = async () => {
     return gulp.src('app/*.html') //копіювання всіх html файлів з папки app
     .pipe(gulp.dest('public')); //виведення в public
 }
 
 exports.cloneCSS = async () => {
-    return gulp.src('app/*main.css') //копіювання всіх html файлів з папки app
+    return gulp.src('app/**/*.css') //копіювання всіх css файлів з папки app
     .pipe(gulp.dest('public')); //виведення в public
 }
+
+exports.cloneFiles = async () => {
+    return gulp.src('app/**/*.js') //копіювання всіх css файлів з папки app
+    .pipe(gulp.dest('public')); //виведення в public
+}
+
+exports.minifyCSS = async () => {
+    return gulp.src('app/**/*.css') //
+    .pipe(cleanCSS()) //мінімізація css 
+    .pipe(gulp.dest('public')); //виведення в public
+}
+
+
+exports.minifyJS = async () => {
+    return gulp.src('app/**/*.js') //
+    .pipe(uglify()) //мінімізація js 
+    .pipe(gulp.dest('public')); //виведення в public
+}
+const cleanCSS = require('gulp-clean-css'); //підключаємо clean-css
+const uglify = require('gulp-uglify'); //підключаємо uglify
